a8cf857f641d8cc8f5d8ca083e87f6356b83635e
V1shvesh <22243137+V1shvesh@users.noreply.github.com>
2018-11-07 18:02:23 +0530

    Define basic redux actions

---
+++ actions/index.js
@@ -0,0 +1,42 @@
+// ACTIONS
+export const ADD_BLOCK = 'ADD_BLOCK';
+export const INIT_LINK = 'INIT_LINK';
+export const SET_LINK = 'SET_LINK';
+export const MOVE_BLOCK = 'MOVE_BLOCK';
+
+// ACTION CREATORS
+export function addBlock(x, y, id) {
+  return {
+    type: ADD_BLOCK,
+    attr: {
+      x,
+      y,
+      id,
+    },
+  };
+}
+
+export function initLink(id) {
+  return {
+    type: INIT_LINK,
+    from: id,
+  };
+}
+
+export function setLink(id) {
+  return {
+    type: SET_LINK,
+    to: id,
+  };
+}
+
+export function moveBlock(x, y, id) {
+  return {
+    type: MOVE_BLOCK,
+    attr: {
+      x,
+      y,
+      id,
+    },
+  };
+}
